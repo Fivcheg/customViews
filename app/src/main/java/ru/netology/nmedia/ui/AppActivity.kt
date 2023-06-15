b100f12803fc7ea9f4474cbf44336f1142860285
Виталий <111383991+Fivcheg@users.noreply.github.com>
2023-06-16 00:06:46 +0300

    RotateAnim

--- app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
@@ -1,6 +1,20 @@
 package ru.netology.nmedia.ui
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.BounceInterpolator
+import android.view.animation.CycleInterpolator
+import android.view.animation.LinearInterpolator
+import android.view.animation.OvershootInterpolator
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.R
 
@@ -8,11 +22,22 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        findViewById<StatsView>(R.id.stats).data = listOf(
-            0.25F,
-            0.25F,
-            0.25F,
+        val view = findViewById<StatsView>(R.id.stats)
+        view.postDelayed({
+            view.data = listOf(
+                0.25F,
+                0.25F,
+                0.25F,
+                0.25F,
+            )
+        }, 1000)
 
-        )
-    }
+        view.animate()
+            .rotation(360F)
+            .setInterpolator(AccelerateInterpolator())
+            .setStartDelay(500)
+            .setDuration(1000)
+            .start()
+
+  }
 }
